0409042811132d15bd3b216e9cccdcff2cc94ac3
root <root@srv1070754.hstgr.cloud>
2025-12-31 03:09:17 +0000

    updates to package-lock, package.json, db.ts, index.ts. .env to work with supabae database

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
